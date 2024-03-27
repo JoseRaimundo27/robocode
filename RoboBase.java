4fcf06e577d60d1798c615e84ca368d89ee5c930
José PC <joseraimundoalves@hotmail.com>
2024-03-27 11:57:05 -0300

    funcionalizando e estruturando o codigo

--- RoboBase.java
+++ RoboBase.java
@@ -5,9 +5,14 @@ import java.awt.Color;
 public class RoboBase extends AdvancedRobot
 {
 	public void run() {
+	
 		setColors(Color.red, Color.black, Color.black,Color.red, Color.green);
 
 		while(true) {
+		//System.out.println("Height:" + getBattleFieldHeight());
+		//System.out.println("Width:" + getBattleFieldWidth());
+		//System.out.println("X:" + getX());
+	//	System.out.println("Y:" + getY());
 			turnRadarRight(20);
 		}
 	}
@@ -16,23 +21,17 @@ public class RoboBase extends AdvancedRobot
 		double distanciaInimigo = e.getDistance();
 		double anguloRadar = getHeading() + e.getBearing() - getRadarHeading();
 		double anguloCanhao = getHeading() + e.getBearing() - getGunHeading();
-		
-		System.out.println(e.getVelocity());
 
 		//Gira o robô em direção ao inimigo
 		setTurnRight(e.getBearing());
 		
          // Gire o radar na direção do inimigo
-        setTurnRadarRight(anguloRadar);
-        
-        // Gire o canhão na direção do inimigo
-        //setTurnGunRight(anguloCanhao);
+        setTurnRadarRight(anguloRadar);   
 		
 		ahead(100);
 		//Se está muito próximo
 		if (distanciaInimigo <= 80) {
-       		setTurnGunRight(anguloCanhao);
-        	fire(3);
+       		algoritmoAtaquePerto(anguloCanhao);
 		}
 		else if (distanciaInimigo <= 200 && distanciaInimigo > 80) {
        		setTurnGunRight(anguloCanhao);
@@ -48,14 +47,50 @@ public class RoboBase extends AdvancedRobot
 		}
 	}
 
-
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+		double anguloBullet = e.getBearing();
+		//System.out.println("Bearing bullet: " + e.getBearing());
+		if(anguloBullet >= -90 && anguloBullet <=90){
+			algoritmoFuga();
+		}
+		//else{
+		//}
+		
 	}
 	
 
 	public void onHitWall(HitWallEvent e) {
-		back(20);
+		checaWall();
 	}	
+	
+	
+	public void onHitRobot(HitRobotEvent e){
+		System.out.println("Bearing hit: " + e.getBearing());
+	
+		double anguloEnemy = e.getBearing();
+		double anguloCanhao = getHeading() + e.getBearing() - getGunHeading();
+		if(anguloEnemy >= -90 && anguloEnemy <=90){
+			algoritmoAtaquePerto(anguloCanhao);
+		}else{
+			algoritmoFuga();
+		}
+	}
+	
+	public void checaWall(){
+		//Checar se os valores estão na faixa permitida para não chocar wall
+	}
+	
+	public void algoritmoFuga(){
+		//Estabelece um algoritmo de fuga para situações adversas
+		ahead(100);
+	}
+	
+	public void algoritmoAtaqueDistancia(){
+		
+	}
+	
+	public void algoritmoAtaquePerto(double anguloCanhao){
+		setTurnGunRight(anguloCanhao);
+        fire(3);
+	}
 }
