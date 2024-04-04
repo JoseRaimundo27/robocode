686845184b162e33ac0f1d2087c0733febd8cab8
José Raimundo <70080687+JoseRaimundo27@users.noreply.github.com>
2024-04-03 22:03:35 -0300

    Preparativos Finais

--- RoboBase.java
+++ RoboBase.java
@@ -18,26 +18,29 @@ public class RoboBase extends AdvancedRobot
 		double distanciaInimigo = e.getDistance();
 		double anguloRadar = getHeading() + e.getBearing() - getRadarHeading();
 		
-		
          // Gire o radar na direção do inimigo
         setTurnRadarRight(anguloRadar);   
 		
 		//Se está muito próximo
 		if (distanciaInimigo <= 100) {
-       		algoritmoAtaque(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(),6,3); //Usar Fire(3) e 6 turnos
+       		algoritmoPreverPosicao(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(),6,3); //Usar Fire(3) e 6 turnos
+			algoritmoPerseguicao(e.getBearing(), e.getDistance(),3);
 		}
 		else if (distanciaInimigo <= 140 && distanciaInimigo > 200) {
-       		algoritmoAtaque(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(),10,2);//Usar Fire(2) e 10 turnos
-   
+       		algoritmoPreverPosicao(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(),10,2);//Usar Fire(2) e 10 turnos
+   			algoritmoPerseguicao(e.getBearing(), e.getDistance(),2);
 		}
 		else if (distanciaInimigo <= 400 && distanciaInimigo > 200) {
-       		algoritmoAtaque(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(), 25,2);//Usar Fire(2) e 25 turnos
+       		algoritmoPreverPosicao(e.getBearing(), e.getHeading(), e.getDistance(),e.getVelocity(), 25,2);//Usar Fire(2) e 25 turnos
+			algoritmoPerseguicao(e.getBearing(), e.getDistance(),2);
 		}
 		else if (distanciaInimigo <= 800 && distanciaInimigo > 400) {
-       		algoritmoAtaque(e.getBearing(),e.getHeading(), e.getDistance(),e.getVelocity(), 33,1); //Usar Fire(1) e 33 turnos
+       		algoritmoPreverPosicao(e.getBearing(),e.getHeading(), e.getDistance(),e.getVelocity(), 33,1); //Usar Fire(1) e 33 turnos
+			algoritmoPerseguicao(e.getBearing(), e.getDistance(),1);
 		}
 		else if (distanciaInimigo <= 1000 && distanciaInimigo > 800) {
-       		algoritmoAtaque(e.getBearing(),e.getHeading(),e.getDistance(),e.getVelocity(), 41,1);//Usar Fire(1) e 41 turnos
+       		algoritmoPreverPosicao(e.getBearing(),e.getHeading(),e.getDistance(),e.getVelocity(), 41,1);//Usar Fire(1) e 41 turnos
+			algoritmoPerseguicao(e.getBearing(), e.getDistance(),1);
 		}
 		
 	
@@ -48,8 +51,13 @@ public class RoboBase extends AdvancedRobot
 		if(anguloBullet >= -90 && anguloBullet <=90){
 			algoritmoFuga();
 		}
+	}
 	
-		
+	public void onHitRobot(HitRobotEvent e){
+		double anguloHit = e.getBearing();
+		if(anguloHit >= -90 && anguloHit <=90){
+			algoritmoFuga();
+		}
 	}
 	
 	public void onHitWall(HitWallEvent e) {
@@ -57,13 +65,7 @@ public class RoboBase extends AdvancedRobot
 	}	
 	
 	
-	public void onHitRobot(HitRobotEvent e){
-		System.out.println("Bearing hit: " + e.getBearing());
-	
-		double anguloEnemy = e.getBearing();
-		double anguloCanhao = getHeading() + e.getBearing() - getGunHeading();
-	
-	}
+
 	
 	public void checaWall(){
 		double positionX = getX();
@@ -75,12 +77,9 @@ public class RoboBase extends AdvancedRobot
 		
 	}
 	
-	public void algoritmoFuga(){
-		//Estabelece um algoritmo de fuga para situações adversas
-		ahead(100);
-	}
+
 	
-	public void algoritmoAtaque(double enemyBearing, double enemyHeading, double distance, double velocity, double turns, double bulletSpeed){
+	public void algoritmoPreverPosicao(double enemyBearing, double enemyHeading, double distance, double velocity, double turns, double bulletSpeed){
 		double a = Rules.getBulletSpeed(bulletSpeed) * turns;
 		double b = velocity * turns;
 		double c = distance;
@@ -98,37 +97,55 @@ public class RoboBase extends AdvancedRobot
 			if(enemyBearing < 0 && (enemyHeading > 0  && enemyHeading < 90) || (enemyHeading > 270  && enemyHeading < 360)){ //Na esquerda e tem q subtrair angulos
 				double sum = enemyBearing - teta;
 				setTurnGunRight(sum);
-				fire(bulletSpeed);
-				System.out.println("1");
+				setFire(bulletSpeed);
 			}
 			else if (enemyBearing < 0 && enemyHeading >= 90  && enemyHeading <=270){
 				double sum = enemyBearing  + teta;
 				setTurnGunRight(sum);
-				fire(bulletSpeed);
-				System.out.println("2");
+				setFire(bulletSpeed);
+			
 			}
 			
 			else if (enemyBearing > 0 && (enemyHeading > 0  && enemyHeading <= 180)){ //Se está a direita
 				double sum = enemyBearing + teta;
 				setTurnGunRight(sum);
-				fire(bulletSpeed);
-				System.out.println("3");
+				setFire(bulletSpeed);
+				
 			}
-			else if (enemyBearing > 0 && (enemyHeading > 180  && enemyHeading < 360)){ //Se está a direita
+			else if (enemyBearing > 0 && (enemyHeading > 180  && enemyHeading <= 360)){ //Se está a direita
 				double sum = enemyBearing - teta;
 				setTurnGunRight(sum);
-				fire(bulletSpeed);
-				System.out.println("4");
+				setFire(bulletSpeed);
+				
 			}
 			
-		} 
-		
-		
+		}else { // inimigo não está se movendo
+			algoritmoPerseguicao(enemyBearing, distance, bulletSpeed);
+			fire(bulletSpeed);
+		}	
 	}
 	
-	public void algoritmoAtaquePerto(double bearing){
-		double anguloCanhao = getHeading() + bearing - getGunHeading();
-		setTurnGunRight(anguloCanhao);
-        setFire(3);
+	public void algoritmoPerseguicao(double enemyBearing, double enemyDistance, double bulletSpeed){
+		setTurnRight(enemyBearing);
+		setAhead(enemyDistance);
+		setTurnGunRight(getHeading() - getGunHeading() +  enemyBearing);
+		fire(bulletSpeed);
+		checaWall();
+	}
+	
+	public void algoritmoFuga(){
+		//Estabelece um algoritmo de fuga para situações adversas
+
+		for(int i = 0; i <= 5; i++){
+			double angulo = Math.random()*360; //Gera angulo aleatorio
+			int anguloInteiro = (int)angulo; //Passa para inteiro
+			
+			double distancia = Math.random()*100; //Gera distancia aleatorio
+			int distanciaInteiro = (int)distancia; //Passa para inteiro
+			setTurnRight(anguloInteiro);
+			setAhead(distanciaInteiro);
+			checaWall();
+		}
+		
 	}
 }
